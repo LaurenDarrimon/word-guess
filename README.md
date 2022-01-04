172f0615a4fe3fe897e1fb6e61970a88bc441aeb
LaurenDarrimon <laurendarrimon@gmail.com>
2022-01-03 22:33:17 -0800

    added javascript to display guesses submitted by user and compare them to the selected answer word

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # word-guess
 Javascript powered word guessing game
 
-plan of work:
 
-1. branch - html framework 
-2. branch - JS 
-3. css
+#### Jump to: 
+* [Description](#description)
+* [Link](#link)
+* [Methods](#methods)
+* [Author](#author)
+
+
+
+## Description
+
+Play a simple word guessing game! When you start the game, a countdown begins. Guess the letters if the word before the timer runs out and you win! Fail to guess all the letters and you lose.  
+
+
+## Link
+
+[Live Word Guessing Game](https://)
+
+
+## Built With
+
+* [Javascript](https://www.w3schools.com/js/)
+* [HTML](https://www.w3schools.com/html/default.asp)
+* [CSS](https://www.w3schools.com/w3css/defaulT.asp)
+
+
+
+## Author
+
+### Lauren Duker Darrimon
+
+- [Link to Portfolio Site](https://www.laurenlalita.com)
+- [Link to Github](https://github.com/LaurenDarrimon)
+- [Link to LinkedIn](https://www.linkedin.com/in/lauren-lalita-duker-9537b1201/)
+
--- script.js
+++ script.js
@@ -4,12 +4,19 @@ let winCount = 0;
 let lossCount = 0;
 let turns = 0;
 
-let currentWord = "";
-let lettersGuessed =[]; //for each turn, all the letters pressed will be stored here and checked against the answer array
+//variables for arrays and words
+let currentWord = ""; //answer for current turn
+let holdEmptySpaces = [] 
+let answerArray = currentWord.split(""); 
+let lettersGuessed = holdEmptySpaces; //for each turn, all the letters pressed will be stored here and checked against the answer array
+//we're starting this array as equal to to the all blanks array, and will replace values as keys come in
 
+//variables that point to DOM nodes
 let startButton = document.querySelector("#start-game-button");
 let countdownDisplay = document.querySelector("#countdown-display");
+let wordGuessField = document.getElementById("word-guess-field");
 let timeMessage = document.querySelector("#time-message");
+
 
 //declare array of possible answer word strings
 let wordBank = ["javascript", "obie", "bunnies", "turtles", "red", "green"];
@@ -21,7 +28,7 @@ startButton.addEventListener("click", playGame);
 
 function playGame(){
     startCountdown(); //one block of code will start & display timer
-    setAnswerField(); //the other block of code will play the game 
+    setAnswerField(); //the other block of code will start playing the game 
 }
 
 //declare function for the timer counting down and rendering time left to screen
@@ -42,19 +49,17 @@ function startCountdown(){
         }, 1000); // function runs once a second (1000 miliseconds)
 };
     
-
+//first game function sets the empty answer field display
 function setAnswerField (){
     //declare answer variable inside the function
-    let answerString = wordBank[turns];
+    currentWord = wordBank[turns];
 
-    console.log (answerString);
+    console.log (currentWord);
     //use split method the answer variable into an array of letter strings
-    let answerArray = answerString.split("");
+    answerArray = currentWord.split("");
 
-    let wordGuessField = document.getElementById("word-guess-field");
-    let holdEmptySpaces = []
 
-    //loop through every letter in the answer array. If the key has been pressed, 
+    //loop through every letter in the answer array to display that number of blanks
     for(i = 0; i < answerArray.length;  i++ ){
         holdEmptySpaces.push("_");
     }
@@ -63,21 +68,69 @@ function setAnswerField (){
 
     wordGuessField.textContent = holdEmptySpaces.join(" ");
 
-    turns++;
-
-
-//run code to listen for keys and check answer
 }
 
-//decalre a function to listen for keys
-//function keyInput(){
-    //event listeners to check for keyboard events 
-    //run consitional loop if key pressed down is a part of the answer array, then change the disply attribute to on 
-    //if (){
 
-    //}
+//event listener for keys 
+document.addEventListener("keydown", function(keyPressEvent) {
+    let keyGuess = keyPressEvent.key.toLowerCase(); 
 
-//}
+    console.log("this key was pressed: " + keyGuess)
+
+    //pass the user's guess to the function
+    compareKeyGuess(keyGuess);
+    }
+);
+
+//declare the function to check the user's key guess against the answer array
+function compareKeyGuess(userKey){
+    if (answerArray.includes(userKey)){
+        console.log("answer includes pressed key")
+        wordGuessField.textContent = "";//empties out the display area of past displays
+        //loop through every letter in the answer array. If the key has been pressed, 
+        for(i = 0; i < answerArray.length;  i++ ){  
+            if(userKey === answerArray[i]){
+                lettersGuessed[i] = userKey; 
+                console.log(lettersGuessed);
+            }
+        }
+       console.log(lettersGuessed);
+        //join the array of empty spaces and guesses into one string and display it
+
+       wordGuessField.textContent = lettersGuessed.join(" ");
+        }
+        checkFinalWord();
+}
+
+//declare a function to check the guesses against the answer
+function checkFinalWord(){
+    if(currentWord === lettersGuessed.join("")){
+        console.log("yay! you've guessed the word!")
+        winning();
+        turns++;
+
+    }
+}
+
+// declare a function for game over
+function gameOver(){
+    console.log("Game Over!");
+    lossCount++;
+    wordGuessField.textContent = "Waa-waah. Sorry, you've lost.";
+
+}
+
+//declare a function for winning
+function winning(){
+    winCount++;
+    wordGuessField.textContent = "Boom shackalacka. You've won.";
+
+}
+
+
+
+
+
 
 
 
